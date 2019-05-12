d54be97e03de429d9ce8c0a5f31a7e0263919300
kvsrinivas91 <kvsrinivas91@gmail.com>
2019-05-12 20:24:06 +0530

    Updated tests

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 import json
@@ -7,127 +7,177 @@ import json
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#Initializing db
-db=SQLAlchemy(app)
+# Initializing db
+db = SQLAlchemy(app)
 
-#Class for pokemon
+# Class for pokemon
 class Pokemon(db.Model):
-    id = db.Column(db.Integer, primary_key=True)# key increases automatically
-    name = db.Column(db.String(40),unique=True,nullable=False)
-    sprite = db.Column(db.String(150),nullable=False)
-    fg = db.Column(db.String(20),nullable=False)
-    bg = db.Column(db.String(20),nullable=False)
-    desc = db.Column(db.String(20),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)  # key increases automatically
+    name = db.Column(db.String(40), unique=True, nullable=False)
+    sprite = db.Column(db.String(150), nullable=False)
+    fg = db.Column(db.String(20), nullable=False)
+    bg = db.Column(db.String(20), nullable=False)
+    desc = db.Column(db.String(20), nullable=False)
 
-    def __init__(self,name,sprite,fg,bg,desc):
-        self.name=name
-        self.sprite=sprite
-        self.fg=fg
-        self.bg=bg
-        self.desc=desc
+    def __init__(self, name, sprite, fg, bg, desc):
+        self.name = name
+        self.sprite = sprite
+        self.fg = fg
+        self.bg = bg
+        self.desc = desc
 
-#Handling Error 404
-@app.errorhandler(404)  
+
+# Handling Error 404
+@app.errorhandler(404)
 def page_not_found(error=None):
-  return ('Error 404'), 404
+    return ("Error 404"), 404
+
 
 @app.errorhandler(500)
 def page_not_found_500(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-#Create a pokemon
-@app.route('/api/pokemon', methods=['POST'])
+
+# Create a pokemon
+@app.route("/api/pokemon", methods=["POST"])
 def pokemon_post():
-    #Getting the json data
-    pokemon=request.json['pokemon']
-    name=pokemon['name']
-    if  len(name)>40 or len(name)<1: #Validating name
-        return("Enter proper name of lenght between 1 and 40 charecters")
-    sprite=pokemon['sprite']
-    if  len(sprite)>150 or len(sprite)<1: #Validating sprite name
-        return("Enter proper sprite name of lenght between 1 and 150 charecters")
-    fg=pokemon['cardColours']['fg']
-    bg=pokemon['cardColours']['bg']
-    desc=pokemon['cardColours']['desc']
-    new_pokemon=Pokemon(name,sprite,fg,bg,desc)#Data passed from Postman
+    # Getting the json data
+    pokemon = request.json["pokemon"]
+    name = pokemon["name"]
+    if len(name) > 40 or len(name) < 1:  # Validating name
+        return "Enter proper name of lenght between 1 and 40 charecters"
+    sprite = pokemon["sprite"]
+    if len(sprite) > 150 or len(sprite) < 1:  # Validating sprite name
+        return "Enter proper sprite name of lenght between 1 and 150 charecters"
+    fg = pokemon["cardColours"]["fg"]
+    bg = pokemon["cardColours"]["bg"]
+    desc = pokemon["cardColours"]["desc"]
+    new_pokemon = Pokemon(name, sprite, fg, bg, desc)  # Data passed from Postman
 
     db.session.add(new_pokemon)
     db.session.commit()
 
-    pokemon=Pokemon.query.filter(Pokemon.name==name).first()
-    new_pokemon={'pokemon' : {'id' : pokemon.id, 'name' : pokemon.name, 'sprite' : pokemon.sprite, 'cardColours' : {'fg' : pokemon.fg, 'bg' : pokemon.bg, 'desc' : pokemon.desc}}}
-    return jsonify(new_pokemon)
-        
-#Getting Pokemon Deatails
-@app.route('/api/pokemon/<int:id>', methods=['GET'])
+    pokemon = Pokemon.query.filter(Pokemon.name == name).first()
+    new_pokemon = {
+        "pokemon": {
+            "id": pokemon.id,
+            "name": pokemon.name,
+            "sprite": pokemon.sprite,
+            "cardColours": {"fg": pokemon.fg, "bg": pokemon.bg, "desc": pokemon.desc},
+        }
+    }
+    return json.dumps(new_pokemon)
+
+
+# Getting Pokemon Deatails
+@app.route("/api/pokemon/<int:id>", methods=["GET"])
 def pokemon_get(id):
-    if id<=0:
-        return("Enter proper Pokemon id")
-    pokemon=Pokemon.query.get(id)
+    if id <= 0:
+        return "Enter proper Pokemon id"
+    pokemon = Pokemon.query.get(id)
 
-    if pokemon!=None:
-        pokemon_details={'pokemon':{'id':pokemon.id,'name':pokemon.name,'sprite':pokemon.sprite,'cardColours':{'fg':pokemon.fg,'bg':pokemon.bg,'desc':pokemon.desc}}}
-        return jsonify(pokemon_details)
+    if pokemon != None:
+        pokemon_details = {
+            "pokemon": {
+                "id": pokemon.id,
+                "name": pokemon.name,
+                "sprite": pokemon.sprite,
+                "cardColours": {
+                    "fg": pokemon.fg,
+                    "bg": pokemon.bg,
+                    "desc": pokemon.desc,
+                },
+            }
+        }
+        return json.dumps(pokemon_details)
     else:
-        return("Pokeomon not found")
+        return "Pokeomon not found"
 
-#Update the product
-@app.route('/api/pokemon/<int:id>', methods=['PATCH'])
+
+# Update the product
+@app.route("/api/pokemon/<int:id>", methods=["PATCH"])
 def pokemon_patch(id):
-    
-    pokemondb=Pokemon.query.get(id)
-    if pokemondb==None:
-        return("Pokemon not found")
-    pokemon=request.json['pokemon']
 
-    if 'name' in pokemon:
-        if  len(pokemon['name'])>40 or len(pokemon['name'])<1: #Validating name
-            return("Enter proper name of lenght between 1 and 40 charecters")
+    pokemondb = Pokemon.query.get(id)
+    if pokemondb == None:
+        return "Pokemon not found"
+    pokemon = request.json["pokemon"]
+
+    if "name" in pokemon:
+        if len(pokemon["name"]) > 40 or len(pokemon["name"]) < 1:  # Validating name
+            return "Enter proper name of lenght between 1 and 40 charecters"
         else:
-            pokemondb.name=pokemon['name']
+            pokemondb.name = pokemon["name"]
 
-    if 'sprite' in pokemon:        
-        if  len(pokemon['sprite'])>150 or len(pokemon['sprite'])<1: #Validating sprite name
-            return("Enter proper sprite name of lenght between 1 and 150 charecters")
-        else:    
-            pokemondb.sprite=pokemon['sprite']
+    if "sprite" in pokemon:
+        if (
+            len(pokemon["sprite"]) > 150 or len(pokemon["sprite"]) < 1
+        ):  # Validating sprite name
+            return "Enter proper sprite name of lenght between 1 and 150 charecters"
+        else:
+            pokemondb.sprite = pokemon["sprite"]
 
-    if 'cardColours' in pokemon:        
-        carColours1=pokemon['cardColours']
-        if 'fg' in carColours1:
-            pokemondb.fg=pokemon['cardColours']['fg']
-        if 'bg' in carColours1:
-            pokemondb.bg=pokemon['cardColours']['bg']
-        if 'desc' in carColours1:    
-            pokemondb.desc=pokemon['cardColours']['desc']
-    
+    if "cardColours" in pokemon:
+        carColours1 = pokemon["cardColours"]
+        if "fg" in carColours1:
+            pokemondb.fg = pokemon["cardColours"]["fg"]
+        if "bg" in carColours1:
+            pokemondb.bg = pokemon["cardColours"]["bg"]
+        if "desc" in carColours1:
+            pokemondb.desc = pokemon["cardColours"]["desc"]
+
     db.session.commit()
 
-    pokemondb=Pokemon.query.filter(Pokemon.id==id).first()
-    new_pokemon={'pokemon' : {'id' : pokemondb.id, 'name' : pokemondb.name, 'sprite' : pokemondb.sprite, 'cardColours' : {'fg' : pokemondb.fg, 'bg' : pokemondb.bg, 'desc' : pokemondb.desc}}}
+    pokemondb = Pokemon.query.filter(Pokemon.id == id).first()
+    new_pokemon = {
+        "pokemon": {
+            "id": pokemondb.id,
+            "name": pokemondb.name,
+            "sprite": pokemondb.sprite,
+            "cardColours": {
+                "fg": pokemondb.fg,
+                "bg": pokemondb.bg,
+                "desc": pokemondb.desc,
+            },
+        }
+    }
     return jsonify(new_pokemon)
 
-#deleting pokemon data
-@app.route('/api/pokemon/<int:id>', methods=['DELETE'])
-def pokemon_delete(id):
-    if id<=0:
-        return("Enter proper Pokemon id")
-    pokemon=Pokemon.query.get(id)
 
-    if pokemon!=None:
-        pokemon_details={'pokemon':{'id':pokemon.id,'name':pokemon.name,'sprite':pokemon.sprite,'cardColours':{'fg':pokemon.fg,'bg':pokemon.bg,'desc':pokemon.desc}}}
+# deleting pokemon data
+@app.route("/api/pokemon/<int:id>", methods=["DELETE"])
+def pokemon_delete(id):
+    if id <= 0:
+        return "Enter proper Pokemon id"
+    pokemon = Pokemon.query.get(id)
+
+    if pokemon != None:
+        pokemon_details = {
+            "pokemon": {
+                "id": pokemon.id,
+                "name": pokemon.name,
+                "sprite": pokemon.sprite,
+                "cardColours": {
+                    "fg": pokemon.fg,
+                    "bg": pokemon.bg,
+                    "desc": pokemon.desc,
+                },
+            }
+        }
         db.session.delete(pokemon)
         db.session.commit()
         return jsonify(pokemon_details)
     else:
-        return("Enter correct pokemon id")
+        return "Enter correct pokemon id"
 
-#Run Server
-if __name__ == '__main__':
+
+# Run Server
+if __name__ == "__main__":
     db.create_all()
-    app.run(host='localhost',debug=True,port=8006)
+    app.run(host="localhost", debug=True, port=8006)
 
-    
--- backend/tests/test_pokemon.py
+++ backend/tests/test_pokemon.py
@@ -8,15 +8,16 @@ sys.path.append("../")
 
 from app import app, db
 
-TEST_DB = "db.sqlite"
+TEST_DB = "test.sqlite"
 
 
 class test_pokemon(unittest.TestCase):
     @classmethod
-    def setUp(self):
+    def setUpClass(self):
         app.config["TESTING"] = True
         app.config["WTF_CSRF_ENABLED"] = False
         app.config["DEBUG"] = False
+        basedir = os.path.abspath(os.path.dirname(__file__))
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
             basedir, TEST_DB
         )
@@ -31,7 +32,7 @@ class test_pokemon(unittest.TestCase):
         print("_____________test pokemon post________________")
         input_data = {
             "pokemon": {
-                "name": "charmamder",
+                "name": "charmander",
                 "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
                 "cardColours": {"fg": "#eeeeee", "bg": "#3e3e3e", "desc": "#111111"},
             }
@@ -49,11 +50,90 @@ class test_pokemon(unittest.TestCase):
             data=json.dumps(input_data),
             content_type="application/json",
         )
-        json_actual_output_data = json.loads(actual_output_data)
-        self.assertEqual(json.dumps(input_data), json_actual_output_data)
-        self.assertEqual(json_actual_output_data.status_code, 200)
+
+        json_actual_output_data = json.loads(actual_output_data.data)
+
+        self.assertEqual(
+            json.dumps(json_actual_output_data), json.dumps(expected_output_data)
+        )
+        self.assertEqual(actual_output_data.status_code, 200)
         print("__________test pokemon post succesfull________")
 
-    if __name__ == "__main__":
-        unittest.main()
+    def test_pokemon_get(self):
+        print("_____________test pokemon get_________________")
+        expected_output_data = {
+            "pokemon": {
+                "id": 1,
+                "name": "charmander",
+                "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+                "cardColours": {"fg": "#eeeeee", "bg": "#3e3e3e", "desc": "#111111"},
+            }
+        }
+
+        actual_output_data = self.app.get("http://localhost:8006/api/pokemon/1")
+        # print(actual_output_data)
+        json_actual_output_data = json.loads(actual_output_data.data)
+        # print(json_actual_output_data)
+        self.assertEqual(
+            json.dumps(json_actual_output_data), json.dumps(expected_output_data)
+        )
+        self.assertEqual(actual_output_data.status_code, 200)
+        print("__________test pokemon get succesfull_________")
+
+    def test_pokemon_patch(self):
+        print("____________Test pokemon patch_______")
+        input_data_to_update = {
+            "pokemon": {
+                "name": "charmander1",
+                "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+                "cardColours": {"fg": "#eeeeee", "bg": "#3e3e3e", "desc": "#111111"},
+            }
+        }
+
+        expected_output_data_patch = {
+            "pokemon": {
+                "id": 1,
+                "name": "charmander1",
+                "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+                "cardColours": {"fg": "#eeeeee", "bg": "#3e3e3e", "desc": "#111111"},
+            }
+        }
+
+        actual_output_data_patch = self.app.patch(
+            "http://localhost:8006/api/pokemo/1n",
+            data=json.dumps(input_data_to_update),
+            content_type="application/json",
+        )
+
+        json_actual_output_data_patch = json.loads(actual_output_data_patch.data)
+
+        self.assertEqual(
+            json.dumps(json_actual_output_data_patch),
+            json.dumps(expected_output_data_patch),
+        )
+        self.assertEqual(actual_output_data_patch.status_code, 200)
+
+        print("____________Test pokemon patch succesfull_______")
+
+    def test_pokemon_delete(self):
+        print("____________Test pokemon  succesfull_______")
+        input_data1 = {
+            "pokemon": {
+                "name": "charmander1",
+                "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+                "cardColours": {"fg": "#eeeeee", "bg": "#3e3e3e", "desc": "#111111"},
+            }
+        }
+
+        actual_output1 = self.app.delete("http://localhost:8006/api/pokemon/1")
+        json_actual1 = json.loads(actual_output_data_patch.data)
+
+        self.assertEqual(json.dumps(input_data1), json.dumps(json_actual1))
+        self.assertEqual(actual_output1.status_code, 200)
+
+        print("____________Test pokemon delete succesfull_______")
+
+
+if __name__ == "__main__":
+    unittest.main()
 
